f79bdcd03f2b7271e927d6a938d176e26af69e1f
arthur <ajcvanittersum@gmail.com>
2025-01-15 16:19:43 +0100

    connected towerplacer and tilemap
    I joined the tower placer with the tilemap, so the towers are only placed on tiles that are allowed

--- Gacha Hell/Assets/Scripts/TowerPlacer.cs
+++ Gacha Hell/Assets/Scripts/TowerPlacer.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TowerPlacer : MonoBehaviour
 {
     public TowerBase[ ]towerOptions;
     private TowerBase currentlySelectedTower;
     public DisplayIntValue valueDisplay;
+    public Tilemap theTilemap;
+    public TileBase[ ]tileBase = new TileBase[4];
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +25,14 @@ public class TowerPlacer : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             Physics.Raycast(ray, out hit, 10f);
-            Instantiate(currentlySelectedTower, hit.point, Quaternion.identity, transform);
-            valueDisplay.ChangeValue(-currentlySelectedTower.cost);
+            if (theTilemap.GetTile(theTilemap.WorldToCell(hit.point)) == tileBase[1])
+            {
+                Instantiate(currentlySelectedTower, hit.point, Quaternion.identity, transform);
+                valueDisplay.ChangeValue(-currentlySelectedTower.cost);
+            }
+            
+            
+            
         }
     }
 }
