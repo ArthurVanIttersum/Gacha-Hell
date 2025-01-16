277d5e6f43415462442c2726b9ea62afc8a4faea
arthur <ajcvanittersum@gmail.com>
2025-01-16 15:28:21 +0100

    Added tower selection and placement
    Added tower selection and placement. tested it by adding a new tower prefab

--- Gacha Hell/Assets/Scripts/TowerPlacer.cs
+++ Gacha Hell/Assets/Scripts/TowerPlacer.cs
@@ -6,20 +6,23 @@ using UnityEngine.Tilemaps;
 public class TowerPlacer : MonoBehaviour
 {
     public TowerBase[ ]towerOptions;
-    private TowerBase currentlySelectedTower;
+    private TowerBase currentlySelectedTower = null;
     public DisplayIntValue valueDisplay;
     public Tilemap theTilemap;
     public TileBase[ ]tileBase = new TileBase[4];
     // Start is called before the first frame update
     void Start()
     {
-        //placeholder
-        currentlySelectedTower = towerOptions[0];
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentlySelectedTower == null)
+        {
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -33,6 +36,26 @@ public class TowerPlacer : MonoBehaviour
             
             
             
+        }
+    }
+
+    public void SetCurrentTower(string towerName)
+    {
+        if (towerName == "Tower1")//placeholder name change this later so it makes sense
+        {
+            currentlySelectedTower = towerOptions[0];
+        }
+        else if (towerName == "Tower2")
+        {
+            currentlySelectedTower = towerOptions[1];
+        }
+        else if (towerName == "Tower3")
+        {
+            currentlySelectedTower = towerOptions[2];
+        }
+        else
+        {
+            Debug.Log("invalid tower name");
         }
     }
 }
