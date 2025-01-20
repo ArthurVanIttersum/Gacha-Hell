156063a0a36e781e828c472911b286be54b36595
Daniël Jansen <danielrnjansen@gmail.com>
2025-01-20 14:42:06 +0100

    Added preview to placing towers

--- Gacha Hell/Assets/Scripts/TowerPlacer.cs
+++ Gacha Hell/Assets/Scripts/TowerPlacer.cs
@@ -14,6 +14,10 @@ public class TowerPlacer : MonoBehaviour
     private bool hoveringOverButton;
     public LayerMask layerMask;
 
+    public Material previewMaterial; // Grayed-out material
+    public Material previewMaterialRed; // Red material for invalid placement
+    private GameObject currentPreview; // The current preview object
+
     // A dictionary to keep track of the towers that have been placed
     private Dictionary<Vector3Int, GameObject> placedTowers = new Dictionary<Vector3Int, GameObject>();
 
@@ -32,7 +36,7 @@ public class TowerPlacer : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            currentlySelectedTower = null;
+            ClearPreview();
         }
         if (currentlySelectedTower == null)
         {
@@ -59,33 +63,117 @@ public class TowerPlacer : MonoBehaviour
                 placedTowers[gridPosition] = newTower;
                 
                 playerVariables.playerMoney -= currentlySelectedTower.cost;
-                currentlySelectedTower = null;
+                ClearPreview();
             }
+        }
+
+        if (currentlySelectedTower != null)
+        {
+            UpdatePreviewPosition();
         }
     }
 
     public void SetCurrentTower(string towerName)
     {
-        if (towerName == "Tower1")//placeholder name change this later so it makes sense
+        switch (towerName)
         {
-            currentlySelectedTower = towerOptions[0];
-        }
-        else if (towerName == "Tower2")
-        {
-            currentlySelectedTower = towerOptions[1];
-        }
-        else if (towerName == "Tower3")
-        {
-            currentlySelectedTower = towerOptions[2];
-        }
-        else
-        {
-            Debug.Log("invalid tower name");
+            case "Tower1": // Change tower name so it makes sense
+                currentlySelectedTower = towerOptions[0];
+                ShowPreview();
+            break;
+
+            case "Tower2":
+                currentlySelectedTower = towerOptions[1];
+                ShowPreview();
+            break;
+
+            case "Tower3":
+                currentlySelectedTower = towerOptions[2];
+            break;
+
+            default:
+                Debug.Log("invalid tower name");
+            break;
         }
     }
 
     public void setHoveringOverButton(bool newValue)
     {
         hoveringOverButton = newValue;
+    }
+
+    private void ShowPreview()
+    {
+        // Destroy the current preview
+        if (currentPreview != null)
+        {
+            Destroy(currentPreview);
+        }
+
+        // Create a new preview instance
+        currentPreview = Instantiate(currentlySelectedTower).gameObject;
+        ApplyPreviewMaterial(currentPreview);
+    }
+
+    // Updates the preview position to snap to the grid under the mouse
+    private void UpdatePreviewPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, layerMask))
+        {
+            Vector3Int gridPosition = theTilemap.WorldToCell(hit.point);
+            currentPreview.transform.position = theTilemap.GetCellCenterWorld(gridPosition);
+
+            // Check if the tower can be placed at the current position
+            if (theTilemap.GetTile(gridPosition) == tileBase[1] && playerVariables.playerMoney >= currentlySelectedTower.cost && !placedTowers.ContainsKey(gridPosition))
+            {
+                ApplyPreviewMaterial(currentPreview);
+            }
+            else
+            {
+                ApplyPreviewMaterialRed(currentPreview);
+            }
+        }
+    }
+
+    // Apply the grayed-out material to all renderers in the preview object
+    private void ApplyPreviewMaterial(GameObject preview)
+    {
+        foreach (Renderer renderer in preview.GetComponentsInChildren<Renderer>())
+        {
+            Material[] materials = renderer.materials;
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i] = previewMaterial;
+            }
+            renderer.materials = materials;
+        }
+    }
+
+    private void ApplyPreviewMaterialRed(GameObject preview)
+    {
+        foreach (Renderer renderer in preview.GetComponentsInChildren<Renderer>())
+        {
+            Material[] materials = renderer.materials;
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i] = previewMaterialRed;
+            }
+            renderer.materials = materials;
+        }
+    }
+
+    // Remove the preview when done
+    public void ClearPreview()
+    {
+        if (currentPreview != null)
+        {
+            Destroy(currentPreview);
+            currentPreview = null;
+        }
+
+        currentlySelectedTower = null;
     }
 }
